dd053456bb584d155522b97445dc66462d08b76a
John Newman <rescuecreative@gmail.com>
2019-08-31 16:55:50 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # namerizer
-A robust random name generator
+
+A robust random name generator.
+
+Namerizer provides you with the ability to generate random combinations of adjectives and nouns. It currently contains 3 built-in lists:
+
+- Animal Pals (example random name: "Sleep Owl")
+- Millenial Cringe (example random name: "Lit Mustache")
+- Fancy Pants (example random name: "Honorable Patron")
+
+### How it works
+
+Install it with npm or yarn...
+
+```
+npm -i namerizer
+```
+
+Pull it into your environment...
+
+```
+import namerizer from "namerizer"
+```
+
+Then start generating names!
+
+Each name list has a unique ID. You can grab these dynamically if you want. But each ID is just the list's name in all upper case letters where spaces are replaced with underscores.
+
+```
+namerizer.getListIds()
+// => ["ANIMAL_PALS", "FANCY_PANTS", "MILLENIAL_CRINGE"]
+```
+
+If you want to see the full lists for some reason, you can grab those as well:
+
+```
+namerizer.getLists()
+/* =>
+{
+  ANIMAL_PALS: {
+    id: "ANIMAL_PALS",
+    name: "Animal Pals",
+    personality: ["fun", "cute", "silly"],
+    adjectives: ["Aberrant", "Able", "Active", ...etc],
+    nouns: ["Aardvark", "Alleycat", "Alpaca", ...etc],
+  },
+  ...etc,
+}
+*/
+```
+
+Most importantly, you'll probably want to generate a name. To do that, just pick a list and pass it to `namerizer.generate`. This will return an instance of a `Name` class that will give you a few options on how to output the generated name.
+
+```
+const name = namerizer.generate("ANIMAL_PALS")
+
+name.name()        // => "Sleepy Owl"
+name.camelCased()  // => "sleepyOwl"
+name.underscored() // => "sleepy_owl"
+name.dasherized()  // => "sleepy-owl"
+```
+
+And that's it!
---
+++ index.js
@@ -0,0 +1,82 @@
+var lists = {
+  ANIMAL_PALS: require("./lists/animalpals"),
+  MILLENIAL_CRINGE: require("./lists/millenialcringe"),
+  FANCY_PANTS: require("./lists/fancypants"),
+};
+
+function getLists() {
+  var output = {}
+  for (var i in lists) {
+    if (lists.hasOwnProperty(i)) {
+      output[i] = JSON.parse(JSON.stringify(lists[i]));
+    }
+  }
+  return output;
+}
+
+function getListIds() {
+  var output = []
+  for (var i in lists) {
+    if (lists.hasOwnProperty(i)) {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
+function randomArrayItem(array) {
+  return array[ Math.floor(Math.random() * array.length) ];
+}
+
+function Name(listId) {
+  if (!Object.prototype.hasOwnProperty.call(lists, listId)) {
+    throw new Error("List", listId, "does not exist");
+  }
+
+  this._list = lists[listId];
+  this._adjective = randomArrayItem(this._list.adjectives);
+  this._noun = randomArrayItem(this._list.nouns);
+  this._name = this._adjective + " " + this._noun;
+}
+
+Name.prototype = {
+
+  name: function () {
+    return this._name;
+  },
+
+  camelCased: function () {
+    var wordList = this._name.toLowerCase().split(/\s/);
+    var camelList = [];
+    var word, i;
+
+    for (i = 0; i < wordList.length; i += 1) {
+      word = wordList[i];
+      if (i !== 0 && /[a-z]/.test(word[0])) {
+        word = word[0].toUpperCase() + word.slice(1);
+      }
+      camelList.push(word);
+    }
+
+    return camelList.join("");
+  },
+
+  underscored: function () {
+    return this._name.toLowerCase().replace(/\s+/g, "_");
+  },
+
+  dasherized: function () {
+    return this._name.toLowerCase().replace(/\s+/g, "-");
+  }
+
+};
+
+function generate(listId) {
+  return new Name(listId);
+}
+
+module.exports = {
+  getListIds: getListIds,
+  getLists: getLists,
+  generate: generate,
+};
